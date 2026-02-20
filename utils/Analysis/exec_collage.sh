f7e46ce05130b881094fed10091bd7cdbc5d149e
Shubham Raghuvanshi <raghuvanshi135@gmail.com>
2026-02-20 13:05:23 +0100

    20-02-2026

--- utils/Analysis/exec_collage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-pyrun="/u/sraghu/software/PYTHON3_install/bin/python3.10"
-"$pyrun" "Collage_gasdens.py"
-"$pyrun" "Collage_gastemp.py"
-"$pyrun" "Collage_haloprops.py"
-"$pyrun" "Collage_halosink.py"
-"$pyrun" "Collage_hmf.py"$pyrun" "Collage_phaseplot.py"
